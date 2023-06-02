29ed80102dc8dc62f58f7fe610b5d28c6ac13978
Ashfaqe Ahmed <ashfaqe.sa12@gmail.com>
2023-06-02 20:31:56 +0530

    title added

--- ebayy/src/App.tsx
+++ ebayy/src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import react from 'react'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-       
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="w-max m-auto">
+      <div className="m-12 border-2 border-sky-600 p-4 rounded-2xl font-bold text-2xl text-sky-800 bg-sky-100">Ebay Clone</div>
+      <div className="m-4">Decentralized Ecommerce Marketplace & Cross Platform app</div>
+      <div>Coming up ... </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
